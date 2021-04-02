16c3c902e85b8563be45d35c27027870979afe1a
Hyram Tang <HYRAM.TANG@GMAIL.COM>
2021-04-02 23:08:57 +0800

    09.高级组件用法-04.使用插槽

---
+++ zhihu/09-advance-components-course/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <div class="row">
+        <div class="col-xs-12">
+          <app-quote>
+            <h2>The Quote</h2>
+            <p>A wonderful Quote</p>
+          </app-quote>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Quote from "./components/Quote.vue";
+
+export default {
+  name: "App",
+  components: {
+    appQuote: Quote,
+  },
+};
+</script>
+
+<style>
+</style>
