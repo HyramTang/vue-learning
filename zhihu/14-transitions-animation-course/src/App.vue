83014cb5ad3a7cbfe7c8942c3d4faa762459844e
Hyram Tang <HYRAM.TANG@GMAIL.COM>
2021-04-09 23:33:00 +0800

    14.使用动画和过渡-10.配置初始化加载动画
    - 滑动动画 slider
    - 混合过渡和动画 transition animation

--- zhihu/14-transitions-animation-course/src/App.vue
+++ zhihu/14-transitions-animation-course/src/App.vue
@@ -9,7 +9,13 @@
         </button>
         <br />
         <br />
-        <transition name="fade">
+        <transition appear name="fade">
+          <div class="alert alert-info" v-if="show">This is some info</div>
+        </transition>
+        <transition appear name="slider" type="animation">
+          <div class="alert alert-info" v-if="show">This is some info</div>
+        </transition>
+        <transition appear name="fade">
           <div class="alert alert-info" v-if="show">This is some info</div>
         </transition>
       </div>
@@ -21,7 +27,7 @@
 export default {
   data() {
     return {
-      show: false,
+      show: true,
     };
   },
 };
@@ -33,7 +39,7 @@ export default {
 }
 
 .fade-enter-active {
-  transition: opacity 0.5s;
+  transition: opacity 1s;
 }
 
 .fade-leave {
@@ -41,7 +47,44 @@ export default {
 }
 
 .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
-  transition: opacity 0.5s;
+  transition: opacity 1s;
   opacity: 0;
+}
+
+.slider-enter {
+  opacity: 0;
+  /* transform: translateY(20px); */
+}
+
+.slider-enter-active {
+  animation: slide-in 1s ease-out forwards;
+  transition: opacity 0.5s;
+}
+
+.slider-leave {
+}
+
+.slider-leave-to {
+  animation: slide-out 1s ease-out forwards;
+  transition: opacity 3s;
+  opacity: 0;
+}
+
+@keyframes slide-in {
+  from {
+    transform: translateY(20px);
+  }
+  to {
+    transform: translateY(0);
+  }
+}
+
+@keyframes slide-out {
+  from {
+    transform: translateY(0);
+  }
+  to {
+    transform: translateY(20px);
+  }
 }
 </style>
