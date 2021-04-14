6f30dcd37fee30a61bea3830f126354f2e2f7172
Hyram Tang <HYRAM.TANG@GMAIL.COM>
2021-04-14 22:54:19 +0800

    16.Vue JS 中的路由概念-06.我在哪？定义活动链接

--- zhihu/16-vue-router-course/src/App.vue
+++ zhihu/16-vue-router-course/src/App.vue
@@ -4,15 +4,22 @@
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
         <h1>Routing</h1>
         <hr />
-        <router-view></router-view>
+        <app-header></app-header>
+        <div style="border: 1px solid lightpink">
+          <router-view></router-view>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import HeaderVue from "./components/Header.vue";
 export default {
   data() {},
+  components: {
+    appHeader: HeaderVue,
+  },
 };
 </script>
 
