7a3b4950244d322c13575a33d6d6536190ad48e8
Hyram Tang <HYRAM.TANG@GMAIL.COM>
2021-04-14 22:41:05 +0800

    16.Vue JS 中的路由概念-03.配置和加载路由

---
+++ zhihu/16-vue-router-course/src/router/index.js
@@ -0,0 +1,28 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../components/Home.vue";
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: Home,
+  },
+  {
+    path: "/user",
+    name: "User",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "user" */ "../components/user/User.vue"),
+  },
+];
+
+const router = new VueRouter({
+  routes,
+});
+
+export default router;
