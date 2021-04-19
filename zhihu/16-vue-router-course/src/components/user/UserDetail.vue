8cc9ff223fc9c1af5b63cb4f3c899d9f4409839f
Hyram Tang <HYRAM.TANG@GMAIL.COM>
2021-04-19 20:45:41 +0800

    16.Vue JS 中的路由概念-244.控制卷屏行为：页面定位

--- zhihu/16-vue-router-course/src/components/user/UserDetail.vue
+++ zhihu/16-vue-router-course/src/components/user/UserDetail.vue
@@ -2,15 +2,27 @@
   <div>
     <h3>Some User Details</h3>
     <p>User loaded has ID：{{ $route.params.id }}</p>
-    <router-link
-      tag="button"
-      class="btn btn-primary"
-      :to="{
-        name: 'UserEdit',
-        params: { id: $route.params.id },
-        query: { locale: 'en', q: 100 },
-      }"
+    <router-link tag="button" class="btn btn-primary" :to="link"
       >Edit User</router-link
     >
   </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      link: {
+        name: 'UserEdit',
+        params: {
+          id: this.$route.params.id,
+        },
+        query: {
+          locale: 'en',
+          q: 100,
+        },
+        hash: '#data',
+      },
+    };
+  },
+};
+</script>
--- zhihu/16-vue-router-course/src/router/index.js
+++ zhihu/16-vue-router-course/src/router/index.js
@@ -1,65 +1,74 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
-import Home from "../components/Home.vue";
-import Header from "../components/Header.vue";
-import User from "../components/user/User.vue";
-import UserStart from "../components/user/UserStart.vue";
-import UserDetail from "../components/user/UserDetail.vue";
-import UserEdit from "../components/user/UserEdit.vue";
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Home from '../components/Home.vue';
+import Header from '../components/Header.vue';
+import User from '../components/user/User.vue';
+import UserStart from '../components/user/UserStart.vue';
+import UserDetail from '../components/user/UserDetail.vue';
+import UserEdit from '../components/user/UserEdit.vue';
 
 Vue.use(VueRouter);
 
 const routes = [
   {
-    path: "/",
-    name: "Home",
+    path: '/',
+    name: 'Home',
     components: {
       default: Home,
-      'header-top': Header
+      'header-top': Header,
     },
   },
   {
-    path: "/user",
-    name: "User",
+    path: '/user',
+    name: 'User',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     // component: User,
     components: {
       default: User,
-      'header-bottom': Header
+      'header-bottom': Header,
     },
     // () =>
     //   import(/* webpackChunkName: "user" */ "../components/user/User.vue")
     children: [
       {
         path: '',
-        component: UserStart
+        component: UserStart,
       },
       {
         path: ':id',
-        component: UserDetail
+        component: UserDetail,
       },
       {
         path: ':id/edit',
         component: UserEdit,
-        name: 'UserEdit'
+        name: 'UserEdit',
       },
     ],
   },
   {
     path: '/redirect-me',
-    redirect: { name: 'Home' }
+    redirect: { name: 'Home' },
   },
   {
     path: '*',
     component: () =>
-      import(/* webpackChunkName: "404" */ "../components/404.vue")
-  }
+      import(/* webpackChunkName: "404" */ '../components/404.vue'),
+  },
 ];
 
 const router = new VueRouter({
-  routes,
+  routes, // eslint-disable-next-line
+  scrollBehavior(to, from, savePosition) {
+    if (savePosition) {
+      return savePosition;
+    }
+    if (to.hash) {
+      return { selector: to.hash };
+    }
+    return { x: 0, y: 0 };
+  },
 });
 
 export default router;
