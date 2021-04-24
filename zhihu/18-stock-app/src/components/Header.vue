cff8cb43397a7ac9f802cd545668dda3f6fe8598
Hyram Tang <HYRAM.TANG@GMAIL.COM>
2021-04-24 15:37:30 +0800

    18.最终实训项目-股票交易-271.完成基本 UI

---
+++ zhihu/18-stock-app/src/components/Header.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <nav class="navbar navbar-default navbar-fixed-top">
+      <!-- We use the fluid option here to avoid overriding the fixed width of a normal container within the narrow content columns. -->
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <button
+            type="button"
+            class="navbar-toggle collapsed"
+            data-toggle="collapse"
+            data-target="#bs-example-navbar-collapse-6"
+            aria-expanded="false"
+          >
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Stock Trader</a>
+        </div>
+
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-6">
+          <ul class="nav navbar-nav">
+            <li><a href="#">Portfolio</a></li>
+            <li><a href="#">Stocks</a></li>
+          </ul>
+
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="#">End Day</a></li>
+            <li class="dropdown">
+              <a
+                href="#"
+                class="dropdown-toggle"
+                data-toggle="dropdown"
+                role="button"
+                aria-haspopup="true"
+                aria-expanded="false"
+                >Save & Load <span class="caret"></span
+              ></a>
+              <ul class="dropdown-menu">
+                <li><a href="#">Save Data</a></li>
+                <li><a href="#">Load Data</a></li>
+              </ul>
+            </li>
+            <li><a href="#">Funds: $10000</a></li>
+          </ul>
+        </div>
+        <!-- /.navbar-collapse -->
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
